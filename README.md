e6d39ecd73881932f955215637e022a24c5a8078
Saeed Mohammadi <s.mohammadi@mabnadp.com>
2021-07-31 15:24:03 +0430

    Initialized.

---
+++ README.md
@@ -0,0 +1,6 @@
+
+
+# Amazon S3 Object Storage Client Sample
+## We're using ArvanCloud as S3 object storage in this example as offers 5GB of free object storage as starter plan.
+
+To test the sample project, first register at ArvanCloud.com, Create a new bucket in Storage > Buckets, copy the required information (endpoint and tokens) from Storage > Dashboard and simply put inside Program.cs and see the magic.
---
+++ src/s3/Program.cs
@@ -0,0 +1,68 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace s3
+{
+    class Program
+    {
+        private static IAmazonS3 _client;
+
+        static void Main(string[] args)
+        {
+            var file = "FILE_FULL_PATH";
+            UploadFile(file, CancellationToken.None).GetAwaiter().GetResult();
+
+            Console.WriteLine("Hello World!");
+        }
+
+        private async static Task UploadFile(string filePath, CancellationToken cancellationToken)
+        {
+            string bucketName = "BUCKET_NAME";
+            string awsAccessKey = "ACCESS_KEY";
+            string awsSecretKey = "SECRET_KEY";
+
+            var config = new AmazonS3Config { ServiceURL = "S3_END_POINT" };
+
+            _client = new AmazonS3Client(awsAccessKey, awsSecretKey, config);
+
+            string folderPath = "test/";
+
+            string fileKey = $"{folderPath}{System.IO.Path.GetFileName(filePath)}";
+            if (await CheckExistsAsync(bucketName, fileKey, cancellationToken))
+                await _client.DeleteObjectAsync(bucketName, fileKey, cancellationToken);
+
+            await _client.PutObjectAsync(new PutObjectRequest()
+            {
+                InputStream = new FileInfo(filePath).OpenRead(),
+                BucketName = bucketName,
+                Key = fileKey
+            }, cancellationToken);
+        }
+
+        private static async Task<bool> CheckExistsAsync(string bucketName, string fileKey, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var res315224ponse = await _client.GetObjectMetadataAsync(new GetObjectMetadataRequest()
+                {
+                    BucketName = bucketName,
+                    Key = fileKey
+                }, cancellationToken);
+
+                return true;
+            }
+
+            catch (Amazon.S3.AmazonS3Exception ex)
+            {
+                if (ex.StatusCode == System.Net.HttpStatusCode.NotFound)
+                    return false;
+
+                throw;
+            }
+        }
+    }
+}
